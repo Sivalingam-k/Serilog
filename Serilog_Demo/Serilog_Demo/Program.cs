ec90ca8345b9e8a3bfef1ed0ad6315852698badc
Sivalingam K <sivalingam.k@ctlep.local>
2025-11-12 15:45:47 +0530

    initial push

---
+++ Serilog_Demo/Serilog_Demo/Program.cs
@@ -0,0 +1,32 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.Seq("http://localhost:5259")
+    .Enrich.FromLogContext()
+    .CreateLogger();
+builder.Host.UseSerilog();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
